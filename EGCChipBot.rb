0a4de2ac8249ed5c71adf3943f8b6f661b836906
Mike Axford <m1ari@m1ari.co.uk>
2016-08-17 12:44:29 +0000

    Initial channel logging support
    Will log messages in the channel to a file including messages from the bot
    Rotates the logs daily
    Aiming for similar output to the irssi log format (would allow for reporting tools)

--- EGCChipBot.rb
+++ EGCChipBot.rb
@@ -3,6 +3,7 @@ require 'bundler/setup'
 
 require_relative 'lib/exchanges'
 require_relative 'lib/evergreencoin'
+require_relative 'lib/logger'
 require_relative 'lib/settings'
 
 # Load settings from EGCChipBot.yaml
@@ -19,11 +20,16 @@ bot = Cinch::Bot.new do
       c.port = Settings.irc[:port]
     end
 
+    # TODO Pull from config file
+    #config.plugins.options[Logger] = {
+      #:logdir => "/tmp/logs"
+    #}
+
     c.channels = Settings.irc[:channels]
     c.nick = Settings.irc[:nick]
     c.user = Settings.irc[:user]
     c.realname = Settings.irc[:name]
-    c.plugins.plugins = [EGC, Exchanges]
+    c.plugins.plugins = [EGC, Exchanges, Logger]
   end
 
 end
